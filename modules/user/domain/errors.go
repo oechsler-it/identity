bee3fa6a4d41a61f5622e798503a7b13ac01e37f
Samuel Oechsler <samuel@oechsler.it>
2023-08-06 23:06:59 +0200

    Add permissions to the user domain model

--- modules/user/domain/errors.go
+++ modules/user/domain/errors.go
@@ -3,8 +3,9 @@ package domain
 import "errors"
 
 var (
-	ErrUserAlreadyExists = errors.New("user already exists")
-	ErrUserNotFound      = errors.New("user not found")
-	ErrInvalidPassword   = errors.New("invalid password")
-	ErrAUserExists       = errors.New("a user exists")
+	ErrUserAlreadyExists         = errors.New("user already exists")
+	ErrUserNotFound              = errors.New("user not found")
+	ErrInvalidPassword           = errors.New("invalid password")
+	ErrAUserExists               = errors.New("a user exists")
+	ErrUserDoesNotHavePermission = errors.New("user does not have permission")
 )
--- modules/user/domain/user.go
+++ modules/user/domain/user.go
@@ -1,11 +1,16 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/samber/lo"
+)
 
 type User struct {
 	Id             UserId         `validate:"required"`
 	Profile        Profile        `validate:"required,dive"`
 	HashedPassword HashedPassword `validate:"required"`
+	Permissions    []Permission   `validate:"required"`
 	CreatedAt      time.Time      `validate:"required"`
 	UpdatedAt      time.Time      `validate:"required"`
 }
@@ -21,7 +26,29 @@ func CreateUser(
 		Id:             id,
 		Profile:        profile,
 		HashedPassword: hashedPassword,
+		Permissions:    []Permission{},
 		CreatedAt:      time.Now(),
 		UpdatedAt:      time.Now(),
 	}
+}
+
+// Assertions
+
+func (u *User) MustHavePermission(permission Permission) error {
+	if !lo.Contains(u.Permissions, permission) {
+		return ErrUserDoesNotHavePermission
+	}
+	return nil
+}
+
+// Actions
+
+func (u *User) GrantPermission(permission Permission) {
+	u.Permissions = append(u.Permissions, permission)
+}
+
+func (u *User) RemovePermission(permission Permission) {
+	u.Permissions = lo.Filter(u.Permissions, func(p Permission, _ int) bool {
+		return p != permission
+	})
 }
